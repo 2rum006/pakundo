1e0991d5f774ec7489df8c485393d2289d248d40
2rum006 <yazie1718@gmail.com>
2025-04-21 10:44:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from rich.style import Style
 import pystyle
 from pystyle import Colors, Colorate
 
-from erjohn import Pakundo
+from test import Pakundo
 
 __CHANNEL_USERNAME__ = "ERJOHN"
 __GROUP_USERNAME__   = "ERJOHN"
