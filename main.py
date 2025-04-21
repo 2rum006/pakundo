acff8445b8c2609719d1073ff5253e01fce3e28c
2rum006 <yazie1718@gmail.com>
2025-04-21 10:43:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -110,7 +110,7 @@ def load_client_details():
     print(Colorate.Horizontal(Colors.rainbow, f'Ip Address : {data.get("query")}.'))
     print(Colorate.Horizontal(Colors.rainbow, f'Location   : {data.get("city")} {data.get("regionName")} {data.get("countryCode")}.'))
     print(Colorate.Horizontal(Colors.rainbow, f'Country    : {data.get("country")} {data.get("zip")}.'))
-    print(Colorate.Horizontal(Colors.rainbow, g'===============[ {__CHANNEL_USERNAME__} ]==============='))
+    print(Colorate.Horizontal(Colors.rainbow, f'===============[ {__CHANNEL_USERNAME__} ]==============='))
 
 def interpolate_color(start_color, end_color, fraction):
     start_rgb = tuple(int(start_color[i:i+2], 16) for i in (1, 3, 5))
